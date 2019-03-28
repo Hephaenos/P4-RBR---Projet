b94b220065b73eb52c8e29fe31bdcbcee7bee36f
Renoult Florent <s174525@ic2s120-18.univ-lemans.fr>
2019-03-28 16:16:42 +0100

    changement

---
+++ SDL2/flo/flo_SDL.c
@@ -0,0 +1,222 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_image.h>
+#include <string.h>
+#include <time.h>
+
+/**
+ * \fn SDL_Texture* tex_img_png(char * s, SDL_Renderer* renderer)
+ * \brief Transforme une image PNG en format texture pour pouvoir l'afficher dans la fenêtre SDL
+ * \param s : chemin d'accès vers l'image PNG
+ * \param renderer : le renderer de la fenêtre.
+ * \return Pointeur sur SDL_Texture
+ */
+SDL_Texture* tex_img_png(char * s, SDL_Renderer* renderer){
+
+    SDL_RWops *rwop=SDL_RWFromFile(s, "rb");
+	SDL_Surface *image=IMG_LoadPNG_RW(rwop);
+	if(!image) {
+	     printf("IMG_LoadPNG_RW: %s\n", IMG_GetError());
+	}
+	SDL_Texture *image_btn_tex = SDL_CreateTextureFromSurface(renderer, image); 
+	if(!image_btn_tex){
+		fprintf(stderr, "Erreur à la création du rendu de l'image : %s\n", SDL_GetError());
+		exit(EXIT_FAILURE);
+	}
+	SDL_FreeSurface(image); /* on a la texture, plus besoin de l'image */
+    return image_btn_tex;
+}
+
+/**
+ * \fn SDL_Texture* tex_img_png(char * s, SDL_Renderer* renderer)
+ * \brief Transforme du texte en format texture pour pouvoir l'afficher dans la fenêtre SDL
+ * \param font : chemin d'accès vers la police d'écriture.
+ * \param size : taille de la police
+ * \param s : texte
+ * \param color : couleur pour le texte
+ * \param renderer : le renderer de la fenêtre.
+ * \return Pointeur sur SDL_Texture
+ */
+SDL_Texture *tex_text(char* font,int size, char* s, SDL_Color color, SDL_Renderer* renderer){
+    TTF_Font *policeTitre = NULL;
+	if( (policeTitre = TTF_OpenFont(font, size)) == NULL){
+        fprintf(stderr, "erreur chargement font : %s\n", TTF_GetError());
+        exit(EXIT_FAILURE);
+    }
+    SDL_Surface *texte = TTF_RenderUTF8_Blended(policeTitre, s, color);
+    if(!texte){
+        fprintf(stderr, "Erreur à la création du texte : %s\n", SDL_GetError());
+        exit(EXIT_FAILURE);
+    }
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_Texture *texte_tex = SDL_CreateTextureFromSurface(renderer, texte);
+    if(!texte_tex){
+        fprintf(stderr, "Erreur à la création du rendu du texte : %s\n", SDL_GetError());
+        exit(EXIT_FAILURE);
+    }
+
+    SDL_FreeSurface(texte); /* on a la texture, plus besoin du texte */
+    TTF_CloseFont(policeTitre); /* Doit être avant TTF_Quit() */
+    return texte_tex;
+}
+
+
+
+
+int lancement_jeu(int i){
+    if(i==1){
+        printf("1 \n \n \n");
+    }
+    if(i==0){
+        printf("0 \n \n \n");
+    }
+    return 0;
+}
+
+
+int main(void){
+    /* Initialisation simple */
+    if (SDL_Init(SDL_INIT_VIDEO) != 0 ) {
+        fprintf(stdout,"Échec de l'initialisation de la SDL (%s)\n",SDL_GetError());
+        return -1;
+    } 
+
+	/* Initialisation TTF */
+	if(TTF_Init() == -1) {
+		fprintf(stderr, "Erreur d'initialisation de TTF_Init : %s\n", TTF_GetError());
+		exit(EXIT_FAILURE);
+	}
+    int x,y;
+    //Le pointeur vers la fenetre
+    SDL_Window* pWindow = NULL;
+    //Le pointeur vers la surface incluse dans la fenetre
+    SDL_Renderer *renderer=NULL;
+    SDL_Rect txtDestRect, txtBvnRect, txtMenuRect[2], imgBtnRect, imgBGRect, txt_titre, nbjoueurRect[5];
+
+
+    // Une variable de couleur noire
+    SDL_Color couleurNoire = {0, 0, 0, 0};
+    SDL_Color couleurBlanc = {255, 255, 255, 255};
+
+    /* Création de la fenêtre */
+    pWindow = SDL_CreateWindow("Puissance 4++",SDL_WINDOWPOS_UNDEFINED,
+                               SDL_WINDOWPOS_UNDEFINED,
+                               800,
+                               571,
+                               SDL_WINDOW_SHOWN);
+    
+    if(!pWindow){
+        fprintf(stderr, "Erreur à la création de la fenetre : %s\n", SDL_GetError());
+        exit(EXIT_FAILURE);
+    }
+
+    renderer = SDL_CreateRenderer(pWindow, -1, SDL_RENDERER_ACCELERATED);
+    if(renderer == NULL){
+        fprintf(stderr, "Erreur à la création du renderer\n");
+        exit(EXIT_FAILURE);
+    }
+
+    SDL_Texture *texte_tex = tex_text("ChowFun.ttf",80,"Puissance 4",couleurBlanc,renderer);
+    SDL_Texture *texteMenu_tex[2];
+    
+    texteMenu_tex[0] = tex_text("ChowFun.ttf",20,"Jouer",couleurNoire,renderer);
+    texteMenu_tex[1] = tex_text("ChowFun.ttf",20,"Quitter",couleurNoire,renderer);
+    
+    txtDestRect.x = 200;
+    txtDestRect.y = 10;
+    SDL_QueryTexture(texte_tex, NULL, NULL, &(txtDestRect.w), &(txtDestRect.h));
+
+    SDL_Texture *image_BG_tex = tex_img_png("Puissance4BG.png",renderer);
+    //Chargement de l'image bouton
+    SDL_Texture *image_btn_tex = tex_img_png("button2.png",renderer);
+
+    if( pWindow )
+    {
+        int running = 1, i;
+        while(running) {
+            SDL_Event e;
+            SDL_GetMouseState(&x,&y);
+            while(SDL_PollEvent(&e)) {
+                switch(e.type) {
+                    case SDL_QUIT: running = 0;break;
+                    case SDL_MOUSEBUTTONDOWN:
+                        
+                    case SDL_WINDOWEVENT:
+                        /* Le fond de la fenêtre sera blanc */
+                        //SDL_SetRenderDrawColor(renderer, 24, 124, 58, 255);
+                        SDL_RenderClear(renderer);
+                        
+                        //BACKGROUND
+                        imgBGRect.x = 0;
+                        imgBGRect.y = 0;
+                        
+                        SDL_QueryTexture(image_BG_tex, NULL, NULL, &(imgBGRect.w), &(imgBGRect.h));
+
+                        SDL_RenderCopy(renderer, image_BG_tex, NULL, &imgBGRect);
+                        
+                        /* Ajout du texte en noir */
+                        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+                        
+                        
+                        //SDL_RenderDrawLine(renderer,0,130,1080,130);
+                        
+                        //Positionnement du premier bouton
+                        imgBtnRect.x = 350;
+                        imgBtnRect.y = 200;
+                        txtMenuRect[0].x=378;
+                        txtMenuRect[0].y=208;
+                        txtMenuRect[1].x=370;
+                        txtMenuRect[1].y=305;
+                        SDL_QueryTexture(image_btn_tex, NULL, NULL, &(imgBtnRect.w), &(imgBtnRect.h));
+                        for(i=0;i<2;i++){
+                            if(x<460 && 350<x && y<imgBtnRect.y+55 && imgBtnRect.y<y){
+                                if(e.type == SDL_MOUSEBUTTONDOWN){
+                                    SDL_DestroyWindow(pWindow);
+                                    lancement_jeu(i);
+                                    return 0;
+                                }
+                            }
+                            txt_titre.x = 170;
+                            txt_titre.y = 10;
+                            SDL_QueryTexture(texte_tex, NULL, NULL, &(txt_titre.w), &(txt_titre.h));
+                            SDL_RenderCopy(renderer, texte_tex, NULL, &(txt_titre));
+
+                            SDL_RenderCopy(renderer, image_btn_tex, NULL, &imgBtnRect);
+
+                            SDL_QueryTexture(texteMenu_tex[i], NULL, NULL, &(txtMenuRect[i].w), &(txtMenuRect[i].h));
+                            SDL_RenderCopy(renderer, texteMenu_tex[i], NULL, &(txtMenuRect[i]));
+                            imgBtnRect.y += 90;
+                        }
+                        /*
+                        imgBtnRect.x = 1000;
+                        imgBtnRect.y = 570;
+                        if(song==PLAY){
+                            temp_music = image_pause_tex;
+                            Mix_ResumeMusic();
+                        }else{
+                            temp_music = image_play_tex;
+                            Mix_PauseMusic();
+                        }
+                        SDL_QueryTexture(temp_music, NULL, NULL, &(imgBtnRect.w), &(imgBtnRect.h));
+                        SDL_RenderCopy(renderer, temp_music, NULL, &imgBtnRect);*/
+
+                        /* On fait le rendu ! */
+                        SDL_RenderPresent(renderer);
+                        break;
+                }
+            }
+        }
+    } else {
+        fprintf(stderr,"Erreur de création de la fenêtre: %s\n",SDL_GetError());
+    }
+    
+    //Destruction de la fenetre
+    if(pWindow != NULL) SDL_DestroyWindow(pWindow);
+    return 0;
+
+}
+
